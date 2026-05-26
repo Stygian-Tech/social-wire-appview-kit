0e0c1591ec667baf0957671ccc9c6f7def904b3c
Sam Clemente <sam@theclementes.com>
2026-05-25 23:31:06 -0500

    Rename foundation repos to descriptive names without Stygian prefix.

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@ let package = Package(
   name: "social-wire-appview-kit",
   platforms: [.macOS(.v14)],
   products: [.library(name: "ThinAppViewCore", targets: ["ThinAppViewCore"])],
-  dependencies: [.package(path: "../stygian-core")],
+  dependencies: [.package(path: "../atproto-primitives")],
   targets: [
-    .target(name: "ThinAppViewCore", dependencies: [.product(name: "StygianCore", package: "stygian-core")], path: "Sources/ThinAppViewCore"),
+    .target(name: "ThinAppViewCore", dependencies: [.product(name: "AtprotoPrimitives", package: "atproto-primitives")], path: "Sources/ThinAppViewCore"),
     .testTarget(name: "ThinAppViewCoreTests", dependencies: ["ThinAppViewCore"], path: "Tests/ThinAppViewCoreTests"),
   ]
 )
